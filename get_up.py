ebe642624eb7713990b55cb32bd4b26ce3c36cc4
yihong0618 <zouzou0208@gmail.com>
2023-04-04 00:19:40 +0800

    fix

--- get_up.py
+++ get_up.py
@@ -1,17 +1,20 @@
 import argparse
+import os
 
 import pendulum
 import requests
 from github import Github
+import random
+import openai
+from BingImageCreator import ImageGen
 
 # 14 for test 12 real get up
-GET_UP_ISSUE_NUMBER = 1
-GET_UP_MESSAGE_TEMPLATE = (
-    "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，喝杯咖啡，背个单词，去跑步。\r\n\r\n 今天的一句诗:\r\n {sentence}"
-)
+GET_UP_ISSUE_NUMBER = 12
+GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，喝杯咖啡，背个单词，去跑步。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n  ![image]({link})"
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
 TIMEZONE = "Asia/Shanghai"
+PROMPT = "请帮我把这个句子 `{sentence}` 翻译成英语，请翻译的有诗意一点儿。"
 
 
 def login(token):
@@ -42,17 +45,45 @@ def get_today_get_up_status(issue):
     return is_today
 
 
-def make_get_up_message():
+def make_pic_and_save(sentence_en, bing_cookie):
+    """
+    return the link for md
+    """
+    i = ImageGen(bing_cookie)
+    images = i.get_images(sentence_en)
+    date_str = pendulum.now().to_date_string()
+    new_path = os.path.join("OUT_DIR", date_str)
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+    i.save_images(images, new_path)
+    return random.choice(images)
+
+
+def make_get_up_message(bing_cookie):
     sentence = get_one_sentence()
     now = pendulum.now(TIMEZONE)
     # 3 - 7 means early for me
     is_get_up_early = 3 <= now.hour <= 7
     get_up_time = now.to_datetime_string()
-    body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
+    ms = [{"role": "user", "content": PROMPT.format(sentence=sentence)}]
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=ms,
+    )
+    sentence_en = (
+        completion["choices"][0].get("message").get("content").encode("utf8").decode()
+    )
+    link = make_pic_and_save(sentence_en, bing_cookie)
+    body = GET_UP_MESSAGE_TEMPLATE.format(
+        get_up_time=get_up_time, sentence=sentence, link=link
+    )
+    print(body)
     return body, is_get_up_early
 
 
-def main(github_token, repo_name, weather_message, tele_token, tele_chat_id):
+def main(
+    github_token, repo_name, weather_message, bing_cookie, tele_token, tele_chat_id
+):
     u = login(github_token)
     repo = u.get_repo(repo_name)
     issue = repo.get_issue(GET_UP_ISSUE_NUMBER)
@@ -60,7 +91,7 @@ def main(github_token, repo_name, weather_message, tele_token, tele_chat_id):
     if is_toady:
         print("Today I have recorded the wake up time")
         return
-    early_message, is_get_up_early = make_get_up_message()
+    early_message, is_get_up_early = make_get_up_message(bing_cookie)
     body = early_message
     if weather_message:
         weather_message = f"现在的天气是{weather_message}\n"
@@ -90,6 +121,9 @@ if __name__ == "__main__":
         "--weather_message", help="weather_message", nargs="?", default="", const=""
     )
     parser.add_argument(
+        "--bing_cookie", help="bing_cookie", nargs="?", default="", const=""
+    )
+    parser.add_argument(
         "--tele_token", help="tele_token", nargs="?", default="", const=""
     )
     parser.add_argument(
@@ -100,6 +134,7 @@ if __name__ == "__main__":
         options.github_token,
         options.repo_name,
         options.weather_message,
+        options.bing_cookie,
         options.tele_token,
         options.tele_chat_id,
     )
