618323301cbce101ec82b213fea4a5338992934f
yihong0618 <zouzou0208@gmail.com>
2023-07-20 00:26:50 +0800

    fix: go back

--- get_up.py
+++ get_up.py
@@ -11,7 +11,7 @@ from bardapi import Bard
 
 
 # 14 for test 12 real get up
-GET_UP_ISSUE_NUMBER = 20
+GET_UP_ISSUE_NUMBER = 12 
 GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，喝杯咖啡，背个单词，去跑步。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n"
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
@@ -80,7 +80,7 @@ def make_get_up_message(bing_cookie, bard_token):
     sentence = get_one_sentence()
     now = pendulum.now(TIMEZONE)
     # 3 - 7 means early for me
-    is_get_up_early = 0 <= now.hour <= 25
+    is_get_up_early = 3 <= now.hour <= 7
     get_up_time = now.to_datetime_string()
     ms = [{"role": "user", "content": PROMPT.format(sentence=sentence)}]
     completion = openai.ChatCompletion.create(
@@ -130,17 +130,16 @@ def main(
         issue.create_comment(comment)
         # send to telegram
         if tele_token and tele_chat_id:
-            pass
-            # requests.post(
-            #     url="https://api.telegram.org/bot{0}/{1}".format(
-            #         tele_token, "sendPhoto"
-            #     ),
-            #     data={
-            #         "chat_id": tele_chat_id,
-            #         "photo": link,
-            #         "caption": body,
-            #     },
-            # )
+            requests.post(
+                url="https://api.telegram.org/bot{0}/{1}".format(
+                    tele_token, "sendPhoto"
+                ),
+                data={
+                    "chat_id": tele_chat_id,
+                    "photo": link,
+                    "caption": body,
+                },
+            )
     else:
         print("You wake up late")
 
